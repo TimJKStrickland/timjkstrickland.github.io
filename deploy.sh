a62532056415e39361d3f7d7f2f3fcfe6aff2ea8
Tim JK Strickland <timjkstrickland@gmail.com>
2025-06-19 15:22:47 -0400

    get rid fo deploy

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-echo "Clean previous build"
-jekyll clean
-echo "Building blog"
-jekyll build
-echo "Deploying blog to s3"
-AWS_PROFILE=timjkstrickland aws s3 sync _site s3://timjkstrickland.com
-echo "Invalidate cache"
-AWS_PROFILE=timjkstrickland aws cloudfront create-invalidation --distribution-id E1AF4LAW02CDTE --paths "/*"
